03286a260a8d96c91d76dbb9ed531a7cc0ff613b
박이건 <parkegun@gmail.com>
2021-03-07 20:17:20 +0900

    인터셉터 추가

---
+++ src/main/java/com/gun/simpleboard/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.gun.simpleboard.config;
+
+import com.gun.simpleboard.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor()).excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**");
+    }
+}
